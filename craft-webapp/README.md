7755f062d319bb0d6f583f3e94fac38153303621
Reece Williams <reecepbcups@gmail.com>
2023-08-05 15:15:01 +0200

    WebApp cleanup

--- craft-webapp/README.md
+++ craft-webapp/README.md
@@ -1,16 +1,3 @@
-# Juno Webapp
+# Craft Webapp
 
-<https://juno.reece.sh>
-
-Mint TokenFactory tokens & register your FeeShare contracts with Keplr
-
----
-
-Is it pretty? No - but is functional. I need to learn tailwind / CSS because bleh
-If you would like to spice things up, feel free to make a PR for Styling
-
-P.S. DAOADO will have in the future, this is just a temp product & example
-
-Local: npm run dev
-
-Production: sh run.sh
+Used just for POC signing Txs, nothing pretty. Will fancy it up with tailwind templates later.
--- craft-webapp/src/components/Transactions.svelte
+++ craft-webapp/src/components/Transactions.svelte
@@ -1,40 +1,45 @@
 <script lang="ts">
+	import type { Window as KeplrWindow } from '@keplr-wallet/types';		
 	import type { OfflineAminoSigner } from '@cosmjs/amino';
-	import type { OfflineDirectSigner } from '@cosmjs/proto-signing';
-	
+	import type { OfflineDirectSigner, EncodeObject } from '@cosmjs/proto-signing';	
 	import { SigningStargateClient } from '@cosmjs/stargate';
 
-	import type { EncodeObject } from '@cosmjs/proto-signing';
+	import { success_notification, error_notification } from './Status.svelte';
 
-	import type { Window as KeplrWindow } from '@keplr-wallet/types';
-
-	// notifications
-	import toast, { Toaster, type ToastOptions } from 'svelte-french-toast';
-
-	// TODO: Future switch to a cosmwasm client
-	import { getSigningCosmosClient } from 'juno-network';
-
-	const toast_style: ToastOptions = {
-		position: 'top-right',
-		duration: 6000,
-		style: 'background: #333; color: #fff; width: 15%; font-size: 1.1rem;'
-	};
-
+	// == Configuration ==
 	const rpcEndpoint = 'https://uni-rpc.reece.sh/';
-	const ENDPOINT = 'http://5.161.42.109:4000';
+	const ENDPOINT = 'http://88.99.161.101:4000';
 	const CHAIN_ID = 'uni-6';
-  let fee = { amount: [{ amount: '500', denom: 'ujunox' }], gas: '250000' }; 
+	let fee = { amount: [{ amount: '500', denom: 'ujunox' }], gas: '250000' };
 
-	let pendingTxs: Tx[] = [];
+	// == Types== 
+	interface Tx {
+		server_name: string;
+		amount: string;
+		description: string;
+		to_address: string;
+		tax: {
+			amount: number;
+			address: string;
+		};
+		denom: string;
+		tx_type: string;
+		from_address: string;
+		timestamp: number;
+	}
+
+	// Application Logic
 
 	let userAddress: string;
+	let pendingTxs: Tx[] = [];
+
 	const get_wallet_for_chain = async (
 		chain_id: string
 	): Promise<OfflineAminoSigner | OfflineDirectSigner> => {
 		// open keplr
 		const keplr = window as KeplrWindow;
 		if (keplr === undefined) {
-			toast.error(`Keplr not found`, toast_style);
+			error_notification('Keplr not found');
 			throw new Error('Keplr not found');
 		}
 
@@ -47,25 +52,11 @@
 		return s;
 	};
 
-	// write a function which post a request
 	async function doSignature(txId: string, tx: Tx) {
-		// const url = "";
-		// const data = {
-		//   tx: txId,
-		//   hash: tendermintHash,
-		// };
+		console.log(tx);
 
-		// sign the tx and broadcast
-
-    // log tx
-    console.log(tx);
-
-		const signer = await get_wallet_for_chain(CHAIN_ID);
-		// const client = await getSigningCosmosClient({ rpcEndpoint, signer });      
-		const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, signer); 
-
-		// const txId = tx[0];
-		// const txData = tx[1];
+		const signer = await get_wallet_for_chain(CHAIN_ID);		
+		const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, signer);
 
 		let msgs: EncodeObject[] = [
 			{
@@ -101,27 +92,13 @@
 
 		const res = await client.signAndBroadcast(userAddress, msgs, fee, tx.description);
 
-		if (res !== undefined && res.code !== 0) {
-			toast.error(`Error: ${res.rawLog}`, toast_style);
+		if (res !== undefined && res.code !== 0) {			
+			error_notification(`Error: ${res.rawLog}`)
 		} else {
-			toast.success(`Success: ${res.rawLog}`, toast_style);
-			toast.success(`Success: ${res.transactionHash}`, toast_style);
+			success_notification(`Success: ${res.rawLog}`);
+			success_notification(`Success: ${res.transactionHash}`);
 
-			const url = `${ENDPOINT}/v1/tx/sign/${txId}/${res.transactionHash}`;
-			console.log(url);
-			
-			// request.post(
-			// 	"https://api.crafteconomy.io/v1/tx/sign/" + txID + "/" + tendermintHash,
-			// 	{ json: { key: 'value' } },
-			// 	function (error, response, body) {
-			// 		if (!error && response.statusCode == 200) {
-			// 			console.log(body);
-			// 			window.location.reload();
-			// 		}
-			// 	}
-			// );  
-
-			// do the above in typescript
+			const url = `${ENDPOINT}/v1/tx/sign/${txId}/${res.transactionHash}`;			
 			const otherParams = {
 				headers: {
 					'content-type': 'application/json; charset=UTF-8'
@@ -133,31 +110,15 @@
 				.then((response) => response.json())
 				.then((data) => {
 					console.log(data);
-					// window.location.reload();
+					success_notification(data);
 				})
 				.catch((error) => {
-					console.log(error);
+					error_notification(error);
 				});
 		}
 	}
 
-	interface Tx {
-		server_name: string;
-		amount: string;
-		description: string;
-		to_address: string;
-		tax: {
-			amount: number;
-			address: string;
-		};
-		denom: string;
-		tx_type: string;
-		from_address: string;
-		timestamp: number;
-	}
-
-	function getPending(walletAddr: string): Tx[] {
-		// http://5.161.42.109:4000/v1/tx/all/juno10r39fueph9fq7a6lgswu4zdsg8t3gxlq670lt0
+	function getPending(walletAddr: string): Tx[] {		
 		const url = ENDPOINT + '/v1/tx/all/' + walletAddr;
 		const otherParams = {
 			headers: {
@@ -169,12 +130,14 @@
 		fetch(url, otherParams)
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data);
+				console.log('Found Txs', data);
+
 				pendingTxs = data as Tx[];
 				return pendingTxs;
 			})
 			.catch((error) => {
 				console.log(error);
+				error_notification(error);
 				pendingTxs = [];
 				return [];
 			});
@@ -183,18 +146,11 @@
 	}
 </script>
 
-<main>  
-  <h1>Pending Transactions</h1>
-  <!-- {#if userAddress}
-    <h3 style="margin-left:10px">{userAddress}</h3>
-	{/if} -->
-
-
-
-	<!-- create a connect wallet button -->
-  <!-- put this bottn nex to pending Txs -->
+<main>
+	<h1>Pending Transactions</h1>
+	
 	<button
-    style="margin-left:10px"
+		style="margin-left:10px"
 		on:click={async () => {
 			const signer = await get_wallet_for_chain(CHAIN_ID);
 			userAddress = (await signer.getAccounts())[0].address;
@@ -202,57 +158,50 @@
 			getPending(userAddress);
 		}}>Connect Wallet</button
 	>
-	
-	
 
 	<!-- TODO: make these drop down boxes -->
 	<ul>
-		{#each Object.entries(pendingTxs) as tx}	
-    <div class="container">		      
-      <!-- no style for  -->
-			<ul style="list-style-type: none;">
-				<!-- {#each Object.entries(tx[1]) as txData}
+		{#each Object.entries(pendingTxs) as tx}
+			<div class="container">
+				<!-- no style for  -->
+				<ul style="list-style-type: none;">
+					<!-- {#each Object.entries(tx[1]) as txData}
 					<li>{txData[0]}: {txData[1]}</li>
 				{/each} -->
-        <!-- <li>Server: {tx[1].server_name}</li> -->
-        <li>Description: {tx[1].description}</li>
-        <li>Amount: {tx[1].amount} {tx[1].denom}</li>
-        <!-- if tx[1].tax.amount -->
-        {#if tx[1].tax.amount > 0}
-          <li>Tax: {tx[1].tax.amount} {tx[1].denom}</li>
-        {/if}
-        <button on:click={() => doSignature(tx[0], tx[1])}>Sign {tx[0]}</button>
-			</ul>
-      
-    </div>
+					<!-- <li>Server: {tx[1].server_name}</li> -->
+					<li>Description: {tx[1].description}</li>
+					<li>Amount: {tx[1].amount} {tx[1].denom}</li>
+					<!-- if tx[1].tax.amount -->
+					{#if tx[1].tax.amount > 0}
+						<li>Tax: {tx[1].tax.amount} {tx[1].denom}</li>
+					{/if}
+					<button on:click={() => doSignature(tx[0], tx[1])}>Sign {tx[0]}</button>
+				</ul>
+			</div>
 		{/each}
 	</ul>
-  
-  
-
 </main>
 
-<style>  
-  button {
-    border-radius: 10px;
-    font-weight: bold;
-    padding: 5px;    
-    border: 1px solid black;
-  }
+<style>
+	button {
+		border-radius: 10px;
+		font-weight: bold;
+		padding: 5px;
+		border: 1px solid black;
+	}
 
-  .container {
-    /* box has a thin black border, some box shadow and is a ligher shade of gray*/
-    border: 1px solid black;
-    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
-    background-color: #f1f1f1;    
-    /* box is as wide as the content up to 50% */
-    width: 50%;
-    /* text color is black */
-    color: black;
-    /* hide ul marks */
-    list-style-type: none;
-    /* padding around the box */
-    padding: 10px;
-  }
-
+	.container {
+		/* box has a thin black border, some box shadow and is a ligher shade of gray*/
+		border: 1px solid black;
+		box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
+		background-color: #f1f1f1;
+		/* box is as wide as the content up to 50% */
+		width: 50%;
+		/* text color is black */
+		color: black;
+		/* hide ul marks */
+		list-style-type: none;
+		/* padding around the box */
+		padding: 10px;
+	}
 </style>
