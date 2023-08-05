57a768c26162ab98b6fa9ec878256519be3196e4
Reece Williams <reecepbcups@gmail.com>
2023-08-05 00:44:50 +0000

    remove old comos integration

---
+++ start-api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd api
+
+# screen -S api
+
+# while true, run npm run dev
+while :
+do
+  npm run dev
+done
---
+++ start-web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: Rewrite this in svelte.
+cd craft-webapp
+npm i
+
+# screen -S webapp
+
+# while true, run npm run dev
+while :
+do
+  npm run dev
+done
