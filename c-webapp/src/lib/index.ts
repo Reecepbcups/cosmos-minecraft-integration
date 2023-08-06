094c8e6dfe062dca7a08d35e6e22653a1081c78c
Reece Williams <reecepbcups@gmail.com>
2023-08-06 22:50:40 +0200

    use https api

--- c-webapp/src/lib/index.ts
+++ c-webapp/src/lib/index.ts
@@ -1,7 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 export const rpcEndpoint = "https://uni-rpc.reece.sh/"
-export const ENDPOINT = 'http://88.99.161.101:4000';
+export const ENDPOINT = 'https://craft-api.reece.sh';
 export const CHAIN_ID = 'uni-6';
 
 export const EXPLORER_TX = 'https://testnet.mintscan.io/juno-testnet/txs';
