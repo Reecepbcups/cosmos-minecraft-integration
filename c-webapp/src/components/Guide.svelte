5620ec2074786545ebbb02541d7fb0fb1323b6e9
Reece Williams <reecepbcups@gmail.com>
2023-08-13 10:06:28 -0500

    Link to guide instead of trying to render

--- c-webapp/src/components/Guide.svelte
+++ c-webapp/src/components/Guide.svelte
@@ -1,10 +1,8 @@
-<script lang="ts">	
-	
-	import SvelteMarkdown from 'svelte-markdown'
+<script lang="ts">
+	const url = `https://raw.githubusercontent.com/Reecepbcups/cosmossdk-minecraft-pvp-server/main/GUIDE.md`;
+	const fancyURL = `https://github.com/Reecepbcups/cosmossdk-minecraft-pvp-server/blob/main/GUIDE.md`;
 
 	async function getGithubGuide(): Promise<string> {
-		const url = `https://raw.githubusercontent.com/Reecepbcups/cosmossdk-minecraft-pvp-server/main/GUIDE.md`;		
-
 		// fetch the data from the url and return it
 		const response = await fetch(url);
 		if (response.ok) {
@@ -12,23 +10,30 @@
 			return text;
 		}
 
-		return "Error loading guide from Github...";
-
+		return 'Error loading guide from Github...';
 	}
 </script>
 
 <main>
-	<div class="flex flex-col justify-center items-center mt-32">		
+	<div class="flex flex-col justify-center items-center mt-32">
 		<h1 class="text-4xl font-bold text-gray-900 dark:text-white">Guide</h1>
-		<hr class="w-1/2 mt-2 mb-2 border-gray-300 dark:border-gray-600" />		
+		<hr class="w-1/2 mt-2 mb-2 border-gray-300 dark:border-gray-600" />
 
-		<!-- on load, run getGithubGuide -->
-		{#await getGithubGuide()}
+		<!-- {#await getGithubGuide()}
 			<p>loading...</p>
-		{:then guide}			
-			<SvelteMarkdown source={guide}/>			
+		{:then guide}						
+			<div class="text-white"
+			>{@html marked(guide)}</div>			
 		{:catch error}
 			<p>{error}</p>
-		{/await}
+		{/await} -->
+
+		<a
+			href={fancyURL}
+			target="_blank"
+			rel="noopener noreferrer"
+			class="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+			>View Guide</a
+		>
 	</div>
 </main>
